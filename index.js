5521c0b62ca92047b6bae792f0efdb1fa42a3b9a
kevinkandie <kevinkan500@gmail.com>
2024-11-07 17:23:54 +0300

    used the reducemethod

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+// Using reduce to sum up all battery 
+const totalBatteries = batteryBatches.reduce((sum, batch) => sum + batch, 0);
